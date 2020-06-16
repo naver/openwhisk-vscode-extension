22b7a394c925c84b3bedda66fd2160262f3f08cf
Seonghyun, Oh <oh.seonghyun@navercorp.com>
2020-06-16 11:42:48 +0900

    Add anntation for `--disable-extensions` option

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -28,9 +28,13 @@ async function main() {
 
         // Download VS Code, unzip it and run the integration test
         await runTests({
-          extensionDevelopmentPath,
-          extensionTestsPath,
-          launchArgs: ['--disable-extensions']
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            /**
+             * The `--disable-extensions` option is for disabling other extensions while debugging.
+             * https://code.visualstudio.com/api/working-with-extensions/testing-extension#disabling-other-extensions-while-debugging
+             */
+            launchArgs: ['--disable-extensions'],
         });
     } catch (err) {
         console.error('Failed to run tests');
